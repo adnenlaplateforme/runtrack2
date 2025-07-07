b47fce691e5b4f471cfc339bf6caa03fc5320bbb
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-07 12:08:37 +0200

    add saut variable

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,15 +1,17 @@
 <?php
+$saut = "<br>";
 $str = "LaPlateforme";
-echo $str."<br>";
+echo $str.$saut;
 $str2 = "Vive";
 $str3 = "!";
-echo " ".$str2." ".$str." ".$str3."<br>";
+echo " ".$str2." ".$str." ".$str3.$saut;
 $val = 6;
-echo "val= ".$val."<br>";
+echo "val= ".$val.$saut;
 $res = "resultat de val + 4 =".$val + 4;
-echo $res."<br>";
+echo $res . $saut;
 $myBool = true;
-echo "contenu de mybool= ".$myBool."<br>";
+echo "contenu de mybool= ". $myBool . $saut;
 $myBool = false;
 echo "contenu de mybool= ".$myBool;
+include "header.html";
 ?>
