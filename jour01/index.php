7bc28432ff802ae596c646518abc5a4c45efcf76
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-07 11:07:45 +0200

    job01 ok

---
+++ jour01/index.php
@@ -0,0 +1,16 @@
+<?php
+$str = "LaPlateforme";
+echo $str."<br>";
+$str2 = "Vive";
+$str3 = "!";
+echo " ".$str2." ".$str." ".$str3."<br>";
+$val = 6;
+echo "val= ".$val."<br>";
+$res = "resultat de val + 4 =".$val + 4;
+echo $res."<br>";
+$myBool = true;
+echo "contenu de mybool= ".$myBool."<br>";
+$myBool = false;
+echo "contenu de mybool= ".$myBool;
+
+?>
