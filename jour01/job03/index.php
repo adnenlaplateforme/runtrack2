b9e9bf64988ade5f78ad0520fa3616afc46a8097
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-07 12:07:32 +0200

    add table.html

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -7,31 +7,5 @@ $val3 = 3;
 echo $val3." = est un entier<br>";
 $val4 = 3.5;
 echo $val4."= est un type float";
-echo "
-<table>
-<thead>
-<tr>
-<th scope='col'>type</th>
-<th scope='col'>nom</th>
-<th scope='col'>valeur</th>
-</tr>
-<tbody>
-<tr>
-<th>String</th>
-<th>val2</th>
-<th> $val2 </th>
-</tr>
-<tr>
-<th>Int</th>
-<th>val3</th>
-<th> $val3 </th>
-</tr>
-<tr>
-<th>Float</th>
-<th>val4</th>
-<th> $val4 </th>
-</tr>
-</tbody>
-</thead>
-</table>"
+include "table.html";
 ?>
