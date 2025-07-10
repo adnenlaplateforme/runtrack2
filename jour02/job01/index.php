440c1199efb3d00eb553d4a41d12f4430d6d91e3
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-10 15:23:06 +0200

    jour02/job01 ok

---
+++ jour02/job01/index.php
@@ -0,0 +1,7 @@
+<?php
+for ($i = 0; $i <= 1337; $i++) {
+    if ($i === 42) {
+        echo "<span style='text-decoration:underline;'>" . $i . "</span><br>";
+    }
+    echo  "<span>" . $i . "</span><br>";
+}
