0a8302d9fc72b4cc57a4de7ee0235534649e07d3
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-11 15:00:43 +0200

    jour03/job07 ok

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -1,2 +1,12 @@
 <?php
+function replaceDecalage($str)
+{
+    $tab = str_split($str);
+    $res = '';
+    for ($i = 1; $i < count($tab); $i++) {
+        $res = $res . $tab[$i];
+    }
+    echo $res . $tab[0];
+}
 $str = "Certaines choses changent, et d'autres ne changeront jamais.";
+replaceDecalage($str);
