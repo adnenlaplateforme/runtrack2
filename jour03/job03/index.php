fed4d489001c62eaaa90d601664df6a7e97d220b
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-21 15:48:49 +0200

    function findVoyelle ok without system function

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,14 +1,31 @@
 <?php
-function findVoyelle($n, $tab)
+function findVoyelle1($n)
 {
-    if (in_array($n, $tab)) {
+    $voyelles = 'aeiouyAEIOUY';
+    for ($i = 0; isset($n[$i]); $i++) {
+        for ($j = 0; isset($voyelles[$j]); $j++) {
+            if ($n[$i] === $voyelles[$j]) {
+                echo $n[$i] . '<br>';
+            }
+        }
+    }
+}
+$str = "I'm sorry Dave I'm afraid I can't do that.";
+echo $str . '<br> les voyelles dans cette chaine: <br>';
+findVoyelle1($str);
+
+/* function findVoyelle($n)
+{
+    $voyelles = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+    if (in_array($n, $voyelles)) {
         echo $n . '<br>';
     }
 }
 $str = "I'm sorry Dave I'm afraid I can't do that.";
 echo $str . '<br> les voyelles dans cette chaine: <br>';
 $tabStr = str_split($str);
-$voyelles = ['a', 'e', 'i', 'o', 'u', 'y'];
 foreach ($tabStr as $key => $value) {
-    findVoyelle($value, $voyelles);
+    findVoyelle($value);
 }
+ */
