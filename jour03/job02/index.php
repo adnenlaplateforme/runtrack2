23c5b4f5835340219c39d611116e09cde7917af7
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-21 15:23:33 +0200

    .\jour03\job02\ use isset in for

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -2,10 +2,16 @@
 
 $str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
 
-$tabStr = str_split($str);
-
+for ($i = 0; isset($str[$i]); $i++) {
+    if ($i % 2 === 0) {
+        echo $str[$i];
+    }
+}
+/*echo "<br>";
+ $tabStr = str_split($str);
 foreach ($tabStr as $key => $value) {
     if ($key % 2 == 0) {
         echo $value;
     }
-}
+} */
+ 
