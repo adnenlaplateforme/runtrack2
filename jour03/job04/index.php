1cf79cace15832d6a90c8e65d4824f0f24f5f4ce
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-21 16:18:34 +0200

    function nbrCaractere ok without system function

--- jour03/job04/index.php
+++ jour03/job04/index.php
@@ -1,5 +1,24 @@
 <?php
+
+
+function nbrCaracter($text)
+{
+    $i = 0;
+    $res = 0;
+    for ($i = 0; isset($text[$i]); $i++) {
+        if (($text[$i] !== ' ') && ($text[$i] !== ',')) {
+            $res = $res + 1;
+        }
+    }
+    return $res;
+}
 $str = "Dans l'espace, personne ne vous entend crier";
+echo $str . '<br>';
+echo 'Le nombre de caracteres dans la chaine est :' . nbrCaracter($str);
+
+
+
+/* $str = "Dans l'espace, personne ne vous entend crier";
 $res = trim(str_replace(' ', '', $str));
 echo $str . '<br>';
 function nbrCaracter($text)
@@ -7,4 +26,4 @@ function nbrCaracter($text)
     $res = strlen($text);
     return $res;
 }
-echo 'Le nombre de caracteres dans la chaine est :' . nbrCaracter($res);
+echo 'Le nombre de caracteres dans la chaine est :' . nbrCaracter($res); */
