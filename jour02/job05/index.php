edf9e2b4a93f07c5982517a337ace6f5ea961dea
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-21 11:44:29 +0200

    set and optimise function isnombrepremier

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -5,13 +5,14 @@ function isnombrepremier($a)
     if ($a == 0 || $a == 1) {
         return false;
     }
-    for ($index = 2; $index <= $a; $index++) {
+    for ($index = 2; $index < $a; $index++) {
         if ($a % $index === 0) {
             return false;
         }
-        return true;
     }
+    return true;
 }
+
 
 for ($i = 2; $i <= 1000; $i++) {
     if (isnombrepremier($i)) {
