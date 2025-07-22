8322af664a198279d669e4d0af2e7dc53ed28fbf
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-22 13:02:27 +0200

    job03 ok

---
+++ jour07/job03/index.php
@@ -0,0 +1,21 @@
+<?php
+function getHello()
+{
+    return "Hello la plateforme";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job03</title>
+</head>
+
+<body>
+    <?php echo getHello(); ?>
+</body>
+
+</html>
