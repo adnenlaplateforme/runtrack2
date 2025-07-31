3e372acc39dd2d4079e919b6cc96cce20f30391d
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-31 10:32:30 +0200

    jour02/job06 ok

--- jour02/job06/index.php
+++ jour02/job06/index.php
@@ -1,8 +1,29 @@
 <?php
-function afficheRectangle($largeur, $hauteur)
+echo '<pre style="font-family: monospace">';
+
+$largeur = 20;
+$hauteur = 20;
+
+for ($i = 1; $i <= $hauteur; $i++) {
+    for ($j = 0; $j <= $largeur; $j++) {
+        if ($i == 1 || $i == $hauteur) {
+            echo "_";
+        } elseif ($j == 1 || $j == $largeur) {
+            echo "|";
+        } elseif ($j == 0) {
+            echo "";
+        } else {
+            echo " ";
+        }
+    }
+    echo "<br>";
+}
+
+/* function afficheRectangle($largeur, $hauteur)
 {
     echo
     '<div style=width:' . $largeur . 'px;height:' . $hauteur . 'px;background:red;>' .
         '</div>';
 }
 afficheRectangle(100, 200);
+ */
