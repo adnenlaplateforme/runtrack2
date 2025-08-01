030b3f69da4902b3b987c07fd220cbb6e38ce8fd
adnenlaplateforme <adnen.said@laplateforme.io>
2025-08-01 14:12:43 +0200

    jour01 & jour02 ok

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,17 +1,15 @@
 <?php
 $saut = "<br>";
 $str = "LaPlateforme";
-echo $str.$saut;
+echo $str . $saut;
 $str2 = "Vive";
 $str3 = "!";
-echo " ".$str2." ".$str." ".$str3.$saut;
+echo " " . $str2 . " " . $str . " " . $str3 . $saut;
 $val = 6;
-echo "val= ".$val.$saut;
-$res = "resultat de val + 4 =".$val + 4;
+echo "val= " . $val . $saut;
+$res = "resultat de val + 4 =" . $val + 4;
 echo $res . $saut;
 $myBool = true;
-echo "contenu de mybool= ". $myBool . $saut;
+echo "contenu de mybool= " . $myBool . $saut;
 $myBool = false;
-echo "contenu de mybool= ".$myBool;
-include "header.html";
-?>
+echo "contenu de mybool= " . $myBool;
--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -1,14 +1,13 @@
 <?php
 for ($i = 0; $i <= 100; $i++) {
-    if ($i >= 0 && $i <= 20) {
+    if ($i <= 20) {
         echo '<i>' .  $i . '</i><br>';
-    } else if ($i >= 25 && $i <= 50) {
-        if ($i = 42) {
-            echo "Laplateforme_<br>";
+    }
+    if ($i >= 25 && $i <= 50) {
+        if ($i == 42) {
+            echo 'La Plateforme_<br>';
         } else {
             echo "<span style='text-decoration: underline;'>" . $i . '</span><br>';
         }
-    } else {
-        echo $i . '<br>';
     }
 }
